ebdd56721688b8ba6ff575dd1c21368c714cd36a
Natalman <natalnahm@gmail.com>
2020-11-15 09:09:01 -0600

    Add a new Tablayout to the mainActivity xml file and in its java file we make a use of it by displaying tab with tab titles.

--- app/src/main/java/com/example/viewpager/MainActivity.java
+++ app/src/main/java/com/example/viewpager/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.viewpager;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 import com.example.viewpager.adapters.SectionAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -20,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
         ViewPager2 vp = findViewById(R.id.view_pager);
         vp.setAdapter(new SectionAdapter(this));
         vp.setPageTransformer(new DepthPageTransformer());
+
+        TabLayout tabs = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabs, vp, new TabLayoutMediator.TabConfigurationStrategy() {
+
+            String [] tabTitles = {"Home", "Events", "Accounts"};
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(tabTitles[position]);
+            }
+        }).attach();
     }
 }
