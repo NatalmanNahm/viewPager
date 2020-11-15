69b30f3e204d443eb79ebe5ef039d8335f945212
Natalman <natalnahm@gmail.com>
2020-11-15 08:46:20 -0600

    Add a transition between the elements of the viewpager and imply to in the main activity as well as set an adapter for the viewPager.

--- app/src/main/java/com/example/viewpager/MainActivity.java
+++ app/src/main/java/com/example/viewpager/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.viewpager;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import com.example.viewpager.adapters.SectionAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager2 vp = findViewById(R.id.view_pager);
+        vp.setAdapter(new SectionAdapter(this));
+        vp.setPageTransformer(new DepthPageTransformer());
     }
 }
